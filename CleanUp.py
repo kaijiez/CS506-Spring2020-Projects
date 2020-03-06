f37a1cf84e372b72160f3e44006aae8522613b96
Kaijie Zhou <kaijiez@bu.edu>
2020-03-06 11:49:34 -0500

    save temp changes to merge owner names

--- CleanUp.py
+++ CleanUp.py
@@ -6,35 +6,23 @@ from operator import itemgetter
 import pandas as pd
 
 #read data in batches,
-def readfile():
-    entries=os.listdir("data/")
+def readfile(filename):
+    # entries=os.listdir("data/")
     streets = []
     ownerNames=[]
-    # id=[]
-    for entry in entries:
-        # f2=open("NameAndAddress.txt","w")
-        with open(os.path.join("data/",entry),'r') as f:
 
-            df = pd.read_csv(os.path.join("data/",entry),encoding = "ISO-8859-1")
-            # id= pd.DataFrame(df.objectid)
-            x = pd.DataFrame(df.owner_addr)
-            y = pd.DataFrame(df.owner_name)
-            z = pd.DataFrame(df.owner_city)
-            q = pd.DataFrame(df.owner_stat)
-            result = pd.concat([x,z,q,y], axis=1, join='inner')
-            result = result.values[:3000].tolist()
-            for row in result:
-                streets.append(str(row[0])+" "+str(row[1])+" "+str(row[2]))
-                ownerNames.append(str(row[3]))
+    df = pd.read_csv(os.path.join("data/",filename),encoding = "ISO-8859-1")
 
-        # read only 1 file so far
-        break
-        f.close()
+    x = pd.DataFrame(df.owner_addr)
+    y = pd.DataFrame(df.owner_name)
+    z = pd.DataFrame(df.owner_city)
+    q = pd.DataFrame(df.owner_stat)
+    result = pd.concat([x,z,q,y], axis=1, join='inner')
+    result = result.values[:100].tolist()
+    for row in result:
+        streets.append(str(row[0])+" "+str(row[1])+" "+str(row[2]))
+        ownerNames.append(str(row[3]))
 
-    # for i in range(len(streets)):
-    #     f2.write(streets[i]+ "             "+ownerNames[i]+"\n")
-
-    # f2.close()
     print(list(zip(streets, ownerNames)))
     return list(zip(streets, ownerNames))
 
@@ -44,14 +32,24 @@ def matchAgencyNames(filename,data):
     address = pd.DataFrame(df.Address)
     result = pd.concat([agency,address],axis=1,join='inner')
     result = result.values.tolist()
+    res=[]
 
     choice=agency.values.tolist()
     for tuple in range(len(data)):
-        if (process.extractOne(data[tuple][1], choice) != None):
-            data[tuple][1]= process.extractOne(data[tuple][1], choice)[0]
+        # print((process.extractOne(data[tuple][1], choice)))
 
-    print(data)
-    return data
+        if (process.extractOne(data[tuple][1], choice) != None):
+            if(process.extractOne(data[tuple][1], choice)[1]>50):
+                res.append((process.extractOne(data[tuple][1], choice)[0][0],data[tuple][0]))
+
+    print("printing")
+    f = open("CleanONResult.txt", "w")
+    for key, val in res:
+        f.write(key + "             " + val + "\n")
+    f.close()
+
+    #very long runtime, need optimization
+    return res
 
 
 
@@ -89,10 +87,6 @@ def compareOwnerNames(tuples):
 
 
     # print(tuples)
-    f = open("CleanONResult.txt", "w")
-    for key,val in tuples:
-        f.write(key+ "             "+val+"\n")
-    f.close()
 
     return tuples
 
@@ -148,19 +142,20 @@ def sort_streets(street_list):
 def extractStreetTuple(street):
     return (street[0][1],street[0][0])
 
-def test(str):
-    line=str.split(",")
-    for each in line:
-        if(each[0]!=" "):
-            print(each[0])
+# def test(str):
+#     line=str.split(",")
+#     for each in line:
+#         if(each[0]!=" "):
+#             print(each[0])
 
 def main():
-    data=readfile()
+    data=readfile("original_luc_gt_909.csv")
     streets=sort_streets(data)
-    # print(streets)
+    print("finished sorted street")
     # print(data)
 
     data=compareOwnerNames(streets)
+    print("finished comparing owner names")
     # testdata='""'
     # test(testdata)
     matchAgencyNames("MassGovernmentAgencyList.csv",data)
